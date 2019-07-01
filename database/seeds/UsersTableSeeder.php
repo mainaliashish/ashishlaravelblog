df437e373bd62115da2b15b0e7cc1edaa0587b19
ashishmainalee <ashishmainalee@gmail.com>
2019-07-01 11:37:37 +0545

    First commmit

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,12 +18,6 @@ class UsersTableSeeder extends Seeder
             'admin'         => 1
         ]);
 
-        App\Profile::create([
-            'user_id'          => $user->id,
-            'avatar'           => 'uploads/avatars/1.jpg',
-            'about'            => 'Sunt enim in tempor magna cupidatat sint minim in laboris sit tempor. Lorem ipsum et dolore reprehenderit minim ut sint laboris culpa id incididunt do ut dolor enim magna ut pariatur irure consectetur officia ex nisi mollit laboris ea ea commodo ea do magna aliquip non labore incididunt ullamco nisi incididunt id deserunt occaecat irure cillum elit incididunt ut est duis magna dolor culpa reprehenderit mollit do ea velit ex proident tempor veniam id quis voluptate sit aliquip qui aliqua anim ',
-            'facebook'          => 'facebook.com',
-            'youtube'           => 'youtube.com'
-        ]);
+
     }
 }
