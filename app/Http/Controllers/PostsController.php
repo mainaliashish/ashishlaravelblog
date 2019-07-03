3de4551efb50b121b002b9b0a3db8f0effbc36ad
ashishmainalee <ashishmainalee@gmail.com>
2019-07-03 20:03:13 +0545

    blog completed

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,7 +8,7 @@ use App\Http\Requests\PostUpdateRequest;
 use App\Post;
 use App\Tag;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Auth;
 use Illuminate\Support\Facades\Session;
 
 class PostsController extends Controller
@@ -50,13 +50,13 @@ class PostsController extends Controller
      */
     public function store(PostCreateRequest $request)
     {
-
         $input = $request->all();
 
         $featured = $request->file('featured');
 
         $input['slug'] = str_slug($input['title']);
-        $user_id = Auth::id;
+        $input['user_id'] = Auth::user()->id;
+
 
         if($featured) {
             $name = time() . $featured -> getClientOriginalName();
